3b038431427a09af4fba11a79101826776390c10
Cid <abhishekmondol378@gmail.com>
2025-12-27 18:21:47 +0600

    Create responses.js

---
+++ src/data/responses.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hello: ["Hi!", "Hello!", "Hey there!"],
+  hi: ["Hi!", "Hey!"],
+  howareyou: ["I'm doing great!", "All good here."],
+  name: ["I'm a backend chatbot API."],
+  bye: ["Goodbye!", "See you soon!"],
+
+  default: [
+    "I'm not sure I understand.",
+    "Can you rephrase that?",
+    "Interesting, tell me more."
+  ]
+};

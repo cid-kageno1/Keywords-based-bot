da74f30fa23b97de5287232544b25bfcc4a3f6ad
Cid <abhishekmondol378@gmail.com>
2025-12-27 18:05:17 +0600

    Create response.js

---
+++ response.js
@@ -0,0 +1,19 @@
+const responses = {
+  "hello": ["Hi there!", "Hello!", "Hey! How can I help you?", "Greetings!"],
+  "hi": ["Hi there!", "Hello!", "Hey!"],
+  "hey": ["Hey!", "Hi!", "Hello!"],
+  "how are you": ["I'm doing great, thanks for asking!", "I'm fine. How about you?"],
+  "name": ["I am a simple chatbot.", "You can call me Bot."],
+  "age": ["I don't have an age. I'm a program."],
+  "weather": ["I can't provide real-time weather information."],
+  "thanks": ["You're welcome!", "No problem!", "Glad to help!"],
+  "bye": ["Goodbye!", "See you later!", "It was nice chatting with you!"],
+  "quit": ["Goodbye!", "See you later!"]
+};
+
+const defaultResponses = [
+  "I'm not sure how to respond to that.",
+  "Can you please rephrase?",
+  "I don't understand.",
+  "Tell me more about that."
+];

c0adcbe51b75f192f1992a488609d5d17f098e5f
Cid <abhishekmondol378@gmail.com>
2025-12-27 18:16:35 +0600

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const chatRoutes = require("./routes/chat.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// 🔥 Change here
+app.use("/chat", chatRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Chatbot API is running" });
+});
+
+module.exports = app;
